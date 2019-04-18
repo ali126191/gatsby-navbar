b8f00e2dd265cf001df083452330a9b060b199e3
shrivitthal <shrivitthalhindumandir@gmail.com>
2019-04-17 20:37:21 -0600

    changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
+
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+
+import 'typeface-roboto';
 import "./layout.css"
+
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -33,12 +39,44 @@ const Layout = ({ children }) => (
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
-        >
+        > 
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <AccessAlarmIcon/>
+
+          <footer className="footer">
+
+            <div className="row">
+              <a href="https://www.gatsbyjs.org/docs/" className="col">
+                <p className="icon text">
+                 <AccessAlarmIcon/>
+                </p>
+                <p className="text">Docs</p>
+               </a>
+               <a href="https://www.gatsbyjs.org/tutorial/" className="col">
+                 <p className="icon text">
+                  <AccessAlarmIcon/>
+                 </p>            
+                 <p className="text">Tutorial</p>
+               </a>
+               <a href="https://www.gatsbyjs.org/plugins/" className="col">
+                <p className="icon text">
+                 <AccessAlarmIcon/>
+                </p>
+                <p className="text">plugins</p>
+               </a>
+               <a href="https://www.gatsbyjs.org/blog/" className="col">
+                <p className="icon text">
+                 <AccessAlarmIcon/>
+                </p>           
+                <p className="text">Blog</p>
+               </a>
+               <a href="https://www.gatsbyjs.org/showcase/" className="col">
+                <p className="icon text">
+                  <AccessAlarmIcon/>
+                </p>
+                <p className="text">Showcase</p>
+               </a>
+            </div>
           </footer>
         </div>
       </>
